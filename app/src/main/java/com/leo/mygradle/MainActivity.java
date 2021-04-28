9146b8b817e4f800364d17bb02c068ca82e8361b
LeoDys <18304097708@163.com>
2021-04-28 17:01:06 +0800

    gradle整理

--- app/src/main/java/com/leo/mygradle/MainActivity.java
+++ app/src/main/java/com/leo/mygradle/MainActivity.java
@@ -3,12 +3,26 @@ package com.leo.mygradle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.just.agentweb.AgentWeb;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AgentWeb mAgentWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent((LinearLayout) findViewById(R.id.ll), new LinearLayout.LayoutParams(-1, -1))
+                .useDefaultIndicator()
+                .createAgentWeb()
+                .ready()
+                .go("http://www.citizenmall.com.cn/wap/Index/index");
+//                .go("https://www.baidu.com");
+
     }
 }
